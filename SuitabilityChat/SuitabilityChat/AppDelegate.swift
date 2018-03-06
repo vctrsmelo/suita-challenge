691fd9a0a51d988e0a5f3def9bcf241e66d5a0ba
Victor Melo <victor.contas@outlook.com>
2018-03-05 22:15:35 -0300

    Project creation and swift lint integration.

---
+++ SuitabilityChat/SuitabilityChat/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SuitabilityChat
+//
+//  Created by Victor Melo on 05/03/18.
+//  Copyright © 2018 Victor Melo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
