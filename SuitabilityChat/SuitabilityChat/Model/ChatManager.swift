07c426f9dec705e288f99dc6afceb026ffac7a68
Victor Melo <victor.contas@outlook.com>
2018-03-06 10:10:29 -0300

    APICommunicator is now fileprivate inside ChatManager. Added urlFinish inside APICommunicator for getting profile

--- SuitabilityChat/SuitabilityChat/Model/ChatManager.swift
+++ SuitabilityChat/SuitabilityChat/Model/ChatManager.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2018 Victor Melo. All rights reserved.
 //
 
+import Alamofire
+
 /* A singleton class that keeps the chat information, including user's response and a state machine. It's accessed by API to get current state information.
 */
 class ChatManager {
@@ -40,4 +42,32 @@ class ChatManager {
             completion(response)
         }
     }
+}
+
+
+extension ChatManager {
+
+    /// APICommunicator shall not be used out of ChatManager
+    fileprivate struct APICommunicator {
+        
+        static private let urlMessage = "https://dev-api.oiwarren.com/api/v2/conversation/message"
+        static private let urlFinish = "https://dev-api.oiwarren.com/api/v2/suitability/finish"
+        
+        static func request(completion: @escaping (_ response: APIResponse) -> Void) {
+            
+            let url = (ChatManager.shared.currentState == .final) ? urlFinish : urlMessage
+            
+            Alamofire.request(url, method: .post, parameters: APIRequest().parameters, encoding: JSONEncoding.default).responseJSON { response in
+                guard let data = response.data else { return }
+                
+                do {
+                    let apiResponse = try JSONDecoder().decode(APIResponse.self, from: data)
+                    completion(apiResponse)
+                } catch {
+                    print("Couldn't complete API request: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+
 }
